6e32e3fb69016721de89979670f696148135040e
Kensuke-Mitsuzawa <kensuke.mit@gmail.com>
2015-10-01 21:34:34 +0900

    added function to resize and make pic in grayscale

--- data_process/face_extraction/face_extract.py
+++ data_process/face_extraction/face_extract.py
@@ -6,10 +6,14 @@ __author__ = 'kensuke-mi'
 
 import cv2
 import os
+import Image
+import ImageOps
+import glob
 
 COLOR = (255, 255, 255) #白
 CASCADE_PATH = "/Users/kensuke-mi/.pyenv/versions/anaconda-2.1.0/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml"
 assert os.path.exists(CASCADE_PATH)
+RESIZED_TUPLE = (150, 150)
 
 def detectFace(image):
     image_gray = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
@@ -22,11 +26,16 @@ def detectFace(image):
 
 
 def extract_face(facerect_list, image, path_to_save):
+    """顔の部分を切り出す。ただし、顔は１つの写真に一人しかいない前提なので注意
+
+    :param facerect_list:
+    :param image:
+    :param path_to_save:
+    :return:
+    """
     assert os.path.exists(os.path.dirname(path_to_save))
 
     for rect in facerect_list:
-        #cv2.imwrite('demo.jpg', image[rect])
-        print rect
         x = rect[0]
         y = rect[1]
         w = rect[2]
@@ -35,8 +44,17 @@ def extract_face(facerect_list, image, path_to_save):
         # img[y: y + h, x: x + w]
         cv2.imwrite(path_to_save, image[y:y+h, x:x+w])
 
+        return image[y:y+h, x:x+w]
+
 
 def draw_line_face(facerect_list, image, path_to_save):
+    """顔部分を白い線で囲む
+
+    :param facerect_list:
+    :param image:
+    :param path_to_save:
+    :return:
+    """
     assert os.path.exists(os.path.dirname(path_to_save))
     #検出した顔を囲む矩形の作成
     for rect in facerect_list:
@@ -52,7 +70,61 @@ def main_procedure(imagefile_name, path_to_save):
     image = cv2.imread(imagefile_name)
     facerect_list = detectFace(image)
     if len(facerect_list) > 0:
-        extract_face(facerect_list, image, path_to_save)
+        image_obj = extract_face(facerect_list, image, path_to_save)
+        path_to_save_resized = path_to_save.replace('.jpg', '_resized.jpg')
+        resize_pic_opencv(im=image_obj, save_path=path_to_save_resized, size_tuple=RESIZED_TUPLE, gray_scale=True)
+    else:
+        return False
+
+
+def resize_pic_opencv(im, save_path, size_tuple, gray_scale=True):
+    """画像サイズ調整とグレースケール化。ただし、OpenCV使用
+
+    :param im:
+    :param save_path:
+    :param size_tuple:
+    :param gray_scale:
+    :return:
+    """
+    assert isinstance(size_tuple, tuple)
+    assert os.path.exists(os.path.dirname(save_path))
+    assert isinstance(gray_scale, bool)
+
+    resized_im = cv2.resize(im,size_tuple)
+    if gray_scale==True:
+        processed_img = cv2.cvtColor(resized_im, cv2.cv.CV_BGR2GRAY)
+    else:
+        processed_img = resized_im
+
+    cv2.imwrite(save_path, processed_img)
+
+    return save_path
+
+
+
+def resize_pic_pil(input_path, save_path, size_tuple, gray_scale=True):
+    """画像サイズを変更するコード
+    ただし、PILライブラリを使用
+
+    :param input_path:
+    :param save_path:
+    :param size_tuple:
+    :param gray_scale:
+    :return:
+    """
+    assert isinstance(size_tuple, tuple)
+    assert os.path.exists(input_path)
+    assert os.path.exists(os.path.dirname(save_path))
+    assert isinstance(gray_scale, bool)
+
+    resized_pic = Image.open(input_path).resize(size_tuple)
+    if gray_scale==True:
+        processed_img = ImageOps.grayscale(resized_pic)
+    else:
+        processed_img = resized_pic
+
+    processed_img.save(save_path)
+
 
 
 def example_usage():
@@ -66,12 +138,11 @@ def girls_face_process():
     PATH_INPUT_DIR = '../../extracted/miss_collection/pics'
     PATH_SAVE_DIR = '../../extracted/miss_collection/face'
 
-    path_pics_list = [os.path.join(PATH_INPUT_DIR, path_pic) for path_pic in os.listdir(PATH_INPUT_DIR)]
-    path_save_list = [os.path.join(PATH_SAVE_DIR, path_pic) for path_pic in os.listdir(PATH_INPUT_DIR)]
+    path_pics_list = [path_pic for path_pic in glob.glob('{}/*jpg'.format(PATH_INPUT_DIR))]
+    path_save_list = [input_path.replace(PATH_INPUT_DIR, PATH_SAVE_DIR) for input_path in path_pics_list]
 
     for index, path_input in enumerate(path_pics_list):
         main_procedure(imagefile_name=path_input, path_to_save=path_save_list[index])
-
 
 
 if __name__ == '__main__':
